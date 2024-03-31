0d96cfc65ce8d0a152c8a9cb7adb15df5ff6b88d
ADITYA VERMA <adityav1304@gmail.com>
2024-03-31 17:12:47 +0530

    a

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from 'react';
+import Markdown from './components/Markdown';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <header className="App-header">
+        <Markdown/>
+      </header>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+import React from 'react';
+import './App.css'; // Import the CSS file
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <hr style={{opacity:"20%",marginBottom:"30px",color:"gray"}}/>
+      <p style={{marginBottom:"10px"}}>&copy; 2024 ADITYA VERMA</p>
+    </footer>
+  );
+}
+
+export default Footer;
